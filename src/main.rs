64d9960d2d28f89e870237456ce3cef31d8a0683
munyokii <munyoki912@gmail.com>
2026-05-16 15:02:56 +0300

    Variables, Constants and Shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+// VARIABLES, CONSTANTS, SHADOWING
 fn main() {
-   let name = "Munyoki";
-   println!("Welcome to Rust School, {}.", name);
+   const MAX_VALUE:i32 = 50;
+   println!("{}", MAX_VALUE);
+
+   let mut x:i64 = 5;
+   println!("Display value of x: {}.", x);
+
+   x = 6;
+
+   println!("Display value of x: {}.", x);
 }
