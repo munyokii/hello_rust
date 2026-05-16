b45470eaf513e1d277a4b3660742c69ee7c56236
munyokii <munyoki912@gmail.com>
2026-05-16 15:42:05 +0300

    If statements

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,79 @@
+// -------------UNCOMMENT TO TEST-----------
+
+
 // VARIABLES, CONSTANTS, SHADOWING
-fn variables_const_shadowing() {
-   const MAX_VALUE:i32 = 50;
-   println!("{}", MAX_VALUE);
 
-   let mut x:i64 = 5;
-   println!("Display value of x: {}.", x);
+// fn variables_const_shadowing() {
+//    const MAX_VALUE:i32 = 50;
+//    println!("{}", MAX_VALUE);
 
-   x = 6;
+//    let mut x:i64 = 5;
+//    println!("Display value of x: {}.", x);
 
-   println!("Display value of x: {}.", x);
-}
+//    x = 6;
+
+//    println!("Display value of x: {}.", x);
+// }
+
 
 // DATA TYPES
-fn data_types() {
-   let age: i32 = 20;
-   let price: f64 = 99.5;
-   let is_student: bool = true;
-   let grade: char = 'A';
-   let name: &str = "Shiva";
 
-   println!("Name: {}"
-   , name);
-   println!("Age: {}"
-   , age);
-   println!("Price: {}"
-   , price);
-   println!("Student: {}"
-   , is_student);
-   println!("Grade: {}"
-   , grade);
-}
+// fn data_types() {
+//    let age: i32 = 20;
+//    let price: f64 = 99.5;
+//    let is_student: bool = true;
+//    let grade: char = 'A';
+//    let name: &str = "Shiva";
+
+//    println!("Name: {}"
+//    , name);
+//    println!("Age: {}"
+//    , age);
+//    println!("Price: {}"
+//    , price);
+//    println!("Student: {}"
+//    , is_student);
+//    println!("Grade: {}"
+//    , grade);
+// }
+
 
 // FUNCTIONS WITH PARAMETERS
-fn greet(name:&str) {
-   println!("Halo halo {}.", name);
-}
 
-fn add(a:i32, b:i32) -> i32 {
-   a + b
+// fn greet(name:&str) {
+//    println!("Halo halo {}.", name);
+// }
+
+// fn add(a:i32, b:i32) -> i32 {
+//    a + b
+// }
+
+
+// CONTROL FLOW
+// IF STATEMENTS
+
+fn if_statement() {
+   let marks = 75;
+   if marks >= 90 {
+      println!("Grade A");
+   } else if marks >= 70 {
+      println!("Grade B");
+   } else if marks >= 35 {
+      println!("Pass");
+   } else {
+      println!("Fail");
+   }
+
 }
 
 
 fn main() {
-   variables_const_shadowing();
-   data_types();
-   greet("muli");
+   // variables_const_shadowing();
+   // data_types();
+   // greet("muli");
 
-   let result = add(32, 65);
-   println!("Result is: {}", result);
+   // let result = add(32, 65);
+   // println!("Result is: {}", result);
+
+   if_statement();
 }
