5a145fa2de708f39590947cd5c6240693d5d4199
munyokii <munyoki912@gmail.com>
2026-05-16 15:19:12 +0300

    Data types

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // VARIABLES, CONSTANTS, SHADOWING
-fn main() {
+fn variables_const_shadowing() {
    const MAX_VALUE:i32 = 50;
    println!("{}", MAX_VALUE);
 
@@ -9,4 +9,29 @@ fn main() {
    x = 6;
 
    println!("Display value of x: {}.", x);
+}
+
+// DATA TYPES
+fn data_types() {
+   let age: i32 = 20;
+   let price: f64 = 99.5;
+   let is_student: bool = true;
+   let grade: char = 'A';
+   let name: &str = "Shiva";
+
+   println!("Name: {}"
+   , name);
+   println!("Age: {}"
+   , age);
+   println!("Price: {}"
+   , price);
+   println!("Student: {}"
+   , is_student);
+   println!("Grade: {}"
+   , grade);
+}
+
+fn main() {
+   variables_const_shadowing();
+   data_types();
 }
